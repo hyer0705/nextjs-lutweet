e3855a5fd515d555e13e3874185052a5dd23a5d0
hyer0705 <hyer1205@gmail.com>
2023-11-09 18:40:07 +0900

    Add hover styling to Btn component

--- components/button.tsx
+++ components/button.tsx
@@ -2,7 +2,7 @@
 
 export default function Btn({ text }: { text: string }) {
   return (
-    <button className="bg-gray-900 py-4 rounded-2xl rounded-tr-none text-white">
+    <button className="bg-gray-900 py-4 rounded-2xl rounded-tr-none text-white hover:bg-gray-800">
       {text}
     </button>
   );
