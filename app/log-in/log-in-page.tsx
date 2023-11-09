ea515d8ff7a84d2b7d4c6123a94de924535227e8
hyer0705 <hyer1205@gmail.com>
2023-11-09 21:14:43 +0900

    Add Logo to LogInPage component

--- app/log-in/log-in-page.tsx
+++ app/log-in/log-in-page.tsx
@@ -1,8 +1,11 @@
 "use client";
 
+import React from "react";
 import { useForm, FormProvider } from "react-hook-form";
+import Logo from "../../assets/Lutweet.svg";
 import Input from "../../components/input";
-import { error } from "console";
+import Btn from "../../components/button";
+import Link from "next/link";
 
 export interface ILogInForm {
   email: string;
@@ -15,12 +18,17 @@ export default function LogInPage() {
   };
 
   return (
-    <div className="">
-      <h1 className="text-white">Logo</h1>
-      <div className="bg-white rounded-3xl rounded-tr-none">
+    <div>
+      <div className="flex justify-center mb-12">
+        <Link href="/">
+          <Logo />
+        </Link>
+      </div>
+      <div className="py-9 bg-white rounded-3xl rounded-tr-none">
         <h1 className="text-center text-3xl">Login</h1>
         <FormProvider {...methods}>
           <form
+            className="bg-white px-10 py-8 flex flex-col space-y-4 rounded-3xl rounded-tr-none"
             onSubmit={methods.handleSubmit(onValid, (error) =>
               console.log(error)
             )}
@@ -30,6 +38,7 @@ export default function LogInPage() {
               labelName="Email"
               placeholder="ex) username@email.com"
             />
+            <Btn text="Log in" />
           </form>
         </FormProvider>
       </div>
