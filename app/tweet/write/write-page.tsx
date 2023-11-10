a72e6f5c815eaa4435dc12903dc2aa0fac9ac0e6
hyer0705 <hyer1205@gmail.com>
2023-11-10 20:11:16 +0900

    Add HTML markup and styling to WritePage component

--- app/tweet/write/write-page.tsx
+++ app/tweet/write/write-page.tsx
@@ -6,6 +6,19 @@ export default function WritePage() {
   return (
     <div className="text-white">
       <Header title="Write" />
+      <div className="flex space-x-2">
+        <div className="w-10 h-10 bg-gray-500 rounded-full" />
+        <form className="flex-1 space-y-4">
+          <textarea
+            className="resize-y w-full p-2 bg-inherit rounded-md focus:outline-none"
+            rows={5}
+            placeholder="What is happening?!"
+          />
+          <button className="w-full py-1 bg-white text-black rounded-xl rounded-tr-none">
+            Write
+          </button>
+        </form>
+      </div>
     </div>
   );
 }
