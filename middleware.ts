53ff6f954040b9621f9623b929639c9de2190655
hyer0705 <hyer1205@gmail.com>
2023-11-18 19:15:49 +0900

    Update config matcher

--- middleware.ts
+++ middleware.ts
@@ -22,5 +22,5 @@ export async function middleware(request: NextRequest) {
 }
 
 export const config = {
-  matcher: ["/", "/tweet/:path*"],
+  matcher: ["/((?!api|_next/static|_next/image|favicon.ico).*)"],
 };
